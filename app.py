98e18fd1e8a004812bebf8256809d8066102633f
Chirag (Srce Cde) <chiragr83@gmail.com>
2025-08-19 09:18:01 -0400

    code-fix

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 @dataclass(frozen=True)
